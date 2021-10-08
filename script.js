a8362242d0ccc067607c16c7675102be2d731094
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-10-08 10:10:21 -0500

    Add code with bugs to be found

---
+++ script.js
@@ -0,0 +1,32 @@
+//FIND BUGS
+
+const navbaMenu = document.querySelector('.navbar-menu');
+const openMenu = document.querySelector('.toolbar-button');
+const backdrop = document.querySelector('.backdrop');
+const hamburger = document.querySelector('.hamburger');
+
+function open() {
+  navbarMenu.classList.remove('display-none');
+  backdrop.classList.remove('display-none');
+  hamburger.classList.add('display-none');
+}
+
+openMenu.addEventListener('click', open);
+
+const closeMenu = document.querySelector('.exit-link');
+hamburger = document.getElementById('.hamburger-button');
+
+function close() {
+  navbarMenu.classList.add('display-none');
+  hamburger.classList.remove('display-none');
+}
+
+closeMenu.addEventListener('click', close);
+
+const links = document.querySelectorAll('.menu-option');
+
+for (let i = 0; i < links.length; i ++) {
+  links[i].addEventListener('click', () => {
+    navbarMenu.classList.add('display-none');
+  });
+}
